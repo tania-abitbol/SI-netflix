342e81492bd73777176966b3a7b67f1818fd985b
alexannebrunold <56915489+alexannebrunold@users.noreply.github.com>
2020-04-10 02:28:17 +0200

    base videoplayer (#15)

--- src/js/player.js
+++ src/js/player.js
@@ -1,45 +1,57 @@
 const video = document.querySelector(".video");
+const videoContent = document.querySelector(".video__played");
 const play = document.querySelector(".video__controller--play");
-const volumpos = document.querySelector(".video__controller--dicrease-volume");
-const volumneg = document.querySelector(".video__controller--increase-volume");
+const volumpos = document.querySelector(".video__controller--increase-volume");
+const volumneg = document.querySelector(".video__controller--decrease-volume");
 const replay = document.querySelector(".video__controller--replay");
 const moreSec = document.querySelector(".video__controller--addtime-forward");
 const lessSec = document.querySelector(".video__controller--addtime-backward");
-const bar = document.querySelector(".video__progress__bar");
+const bar = document.querySelector(".video__progress");
+const progressBar = document.querySelector(".video__progress__bar");
+videoContent.volume = 0.2;
 
 play.addEventListener("click", () => {
-  if (video.paused) video.play();
-  else video.pause();
+  if (videoContent.paused) {
+    videoContent.play();
+    play.textContent = "Pause";
+  } else {
+    videoContent.pause();
+    play.textContent = "Play";
+  }
 });
 
 volumpos.addEventListener("click", () => {
-  if (video.volume + 0.1 >= 1) video.volume = 1;
-  else video.volume += 0.1; // 0 = mute, 1 = max
+  if (videoContent.volume + 0.1 <= 1) {
+    videoContent.volume += 0.1;
+  } else {
+    videoContent.volume = 1;
+  }
 });
 
-volumneg.addEventListener("click", () => {
-  if (video.volume - 0.1 <= 0) video.volume = 0;
-  else video.volume -= 0.1; // 0 = mute, 1 = max
-});
+if (videoContent.volume - 0.1 >= 0) {
+  videoContent.volume -= 0.1;
+} else {
+  video.volume = 0; // 0 = mute, 1 = max
+}
 
 replay.addEventListener("click", () => {
-  video.currentTime = 0;
-  video.play();
+  videoContent.currentTime = 0;
 });
 
 moreSec.addEventListener("click", () => {
-  if (video.currentTime + 10 >= video.duration) video.currentTime += 0;
-  else video.currentTime += 10;
-  video.play();
+  videoContent.currentTime += 10;
 });
 
 lessSec.addEventListener("click", () => {
-  if (video.currentTime - 10 <= 0) video.currentTime -= 0;
-  else video.currentTime -= 10;
-  video.play();
+  videoContent.currentTime -= 10;
 });
 
-video.addEventListener("timeupdate", () => {
-  const progress = video.currentTime / video.duration; // 0 = Début, 1 = Fin
-  bar.style.transform = "scaleX(" + progress + ")"; // On applique le nouveau style a notre bar
+videoContent.addEventListener("timeupdate", () => {
+  var vid = videoContent.currentTime;
+  var duration = videoContent.duration;
+  if (videoContent.currentTime !== videoContent.duration) {
+    progressBar.style.width = (vid / duration) * 100 + "%";
+  } else {
+    progressBar.style.width = "0%";
+  }
 });
