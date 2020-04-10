ab240eebee672e4be31af63315d4440bc78cce11
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2020-04-10 11:59:14 +0200

    Landing pagepage (#17)

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,25 @@
-const gridButton = document.querySelector(".grid");
-const slider = document.querySelector(".slider");
-gridButton.addEventListener("click", () => {
-  slider.classList.remove("slider--list");
-  slider.classList.add("slider--grid");
+// const gridButton = document.querySelector(".grid");
+// const slider = document.querySelector(".slider");
+// gridButton.addEventListener("click", () => {
+//   slider.classList.remove("slider--list");
+//   slider.classList.add("slider--grid");
+// });
+
+// const listButton = document.querySelector(".list");
+
+// listButton.addEventListener("click", () => {
+//   slider.classList.add("slider--list");
+//   slider.classList.remove("slider--grid");
+// });
+
+const fleche_g = document.querySelector(".prev");
+const fleche_d = document.querySelector(".next");
+const all = document.querySelector(".slider ");
+
+fleche_d.addEventListener("click", function () {
+  all.scrollLeft += 100;
 });
 
-const listButton = document.querySelector(".list");
-
-listButton.addEventListener("click", () => {
-  slider.classList.add("slider--list");
-  slider.classList.remove("slider--grid");
+fleche_g.addEventListener("click", function () {
+  all.scrollLeft -= 100;
 });
