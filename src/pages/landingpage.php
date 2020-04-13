b04c67f05a1dc097bfc23033ea42024af3ef757b
annie.trn <annietth@users.noreply.github.com>
2020-04-14 00:32:24 +0200

    add all image, html and style of main part, beginning the part one

--- src/pages/landingpage.php
+++ src/pages/landingpage.php
@@ -30,23 +30,131 @@
       </div>
     </nav>
   </header>
-  <div class="">
-  <img src="../assets/images/macbook-pro.jpg"/>
-  </div>
-  <div class="poster">
-    <div class="poster__firstrow">
-  <img src="../assets/poster/marmageddon.jpg"/>
-  <img src="../assets/poster/austin_power2.jpg"/>
-  <img src="../assets/poster/babydriver.jpg"/>
-  <img src="../assets/poster/batman.jpg"/>
-  <img src="../assets/poster/braveheart.jpg"/>
+
+  <div class="main">
+        <div class="main__box1">          
+            <h3 class="main__box1__title">GFX progress</h3>
+            <p>
+              The progress of out platform start around a single one idea, how GFX can be better than the other platform?
+            </p>
+            <p>
+              The answer turn around four fonctionalities which are important for us, for a perfect watching sessions & a magic moment
+            </p>
+            <div class="main__box1__body">
+           
+                <div class="main__box1__body__logo">
+                    <div class="main__box1__body__logo__com">
+                        <img src="../assets/images/engagement/like.svg">
+                        <p>Community</p>
+                    </div>
+                    <div class="main__box1__body__logo__pro">
+                        <img src="../assets/images/engagement/progress.svg">
+                        <p>100% connected</p>
+                    </div>
+                    <div class="main__box1__body__logo__start">
+                        <img src="../assets/images/engagement/replay_start.svg">
+                        <p>Replay Infini</p>
+                    </div>
+                    <div class="main__box1__body__logo__tchat">
+                        <img src="../assets/images/engagement/chat.svg">
+                        <p>Replay Infini</p>
+                    </div>
+                </div>
+            </div>       
+        </div>
+
+        <div class="main__box2">
+            <h3 class="main__box2__title"> Our plateform</h3>
+            <div class="main__box2__text">
+                <p class="main__box2__text__little">We update movies that come from all over the world. For more passion  and more fun. 
+                    We know what you need and what makes you happy !
+                    Don't hesitate, it's time to take the plunge.
+                </p>
+                <img src="../assets/images/mockuup/ipad.svg">
+            </div>
+           
+            
+        <div>
+        
+        <div class="main__box3">
+            <h3 class="main__box3__title">Choose your plan</h3>
+            <div class="main__box3__form">
+                <div class="main__box3__form__plan1">
+                    <p>.99</p>
+                    <p><strong>$5</strong>/mounth</p>
+                    <hr>
+                    <p>Film/Serie illimité</p>
+                    <hr>
+                    <p>1 screen</p>
+                    <hr>
+                    <p>Cancellable at any time</p>
+                    <hr>
+                    <p>First mounth free</p>
+                    <hr>
+                    <p class="main__box3__form__plan1_tip">See more</p>
+                    <div class="main__box3__form__plan1__submit">
+                        <button class="main__box3__form__plan1__submit__button" type="submit">
+                          <a href="#">START</a>
+                        </button>
+                    </div>
+
+                </div>
+                <div class="main__box3__form__plan2">
+                    <p>.99</p>
+                    <p><strong>$11</strong>/mounth</p>
+                    <hr>
+                    <p>Film/Serie illimité</p>
+                    <hr>
+                    <p>2 screen</p>
+                    <hr>
+                    <p>Cancellable at any time</p>
+                    <hr>
+                    <p>First mounth free</p>
+                    <hr>
+                    <p class="main__box3__form__plan1_tip">See more</p>
+                    <div class="main__box3__form__plan2__submit">
+                        <button class="main__box3__form__plan2__submit__button" type="submit">
+                          <a href="#">START</a>
+                        </button>
+                    </div>
+
+                </div>
+                <div class="main__box3__form__plan1">
+                    <p>.99</p>
+                    <p><strong>$14</strong>/mounth</p>
+                    <hr>
+                    <p>Film/Serie illimité</p>
+                    <hr>
+                    <p>4 screen</p>
+                    <hr>
+                    <p>Cancellable at any time</p>
+                    <hr>
+                    <p>Two mounth free</p>
+                    <hr>
+                    <p class="main__box3__form__plan1_tip">See more</p>
+                    <div class="main__box3__form__plan1__submit">
+                        <button class="main__box3__form__plan1__submit__button" type="submit">
+                          <a href="#">START</a>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+      
     </div>
-    <div class="poster__secondrow">
-  <img src="../assets/poster/ironman2.jpg"/>
-  <img src="../assets/poster/red.jpg"/>
-  <img src="../assets/poster/salt.jpg"/>
-  <img src="../assets/poster/the-accountant.jpg"/>
-  <img src="../assets/poster/the-conjuring.jpg"/>
-    </div>
-  </div>
+  <!-- <div class="poster">
+  
+    <img src="../assets/poster/marmageddon.jpg"/>
+    <img src="../assets/poster/austin_power2.jpg"/>
+    <img src="../assets/poster/babydriver.jpg"/>
+    <img src="../assets/poster/batman.jpg"/>
+    <img src="../assets/poster/braveheart.jpg"/>
+    <img src="../assets/poster/ironman2.jpg"/>
+    <img src="../assets/poster/red.jpg"/>
+    <img src="../assets/poster/salt.jpg"/>
+    <img src="../assets/poster/the-accountant.jpg"/>
+    <img src="../assets/poster/the-conjuring.jpg"/>
+    
+  </div> -->
   <?php include("./components/footer.php")?>
