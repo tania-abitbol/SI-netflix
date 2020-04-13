a19d9c19faf090396a4b67879c21a141105031ff
annie.trn <annietth@users.noreply.github.com>
2020-04-13 22:52:25 +0200

    corrected error

--- src/pages/landingpage.php
+++ src/pages/landingpage.php
@@ -1,36 +1,30 @@
-<!DOCTYPE html>
-<html lang="fr">
-
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>GOFILMAX</title>
-  <link rel="stylesheet" href="#" />
+  <link rel="stylesheet" href="../styles/css/style.css"/>
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="js/index.js" defer></script>
 </head>
 
   <body>
-<header class="header">
-    <nav class="header__nav">
-      <div class="header__nav__left">
+<header class="head">
+    <nav class="head__nav">
+      <div class="head__nav__left">
         <img src="../assets/images/logo.png" alt="" />
-        <ul class="header__nav__left__links">
+        <ul class="head__nav__left__links">
           <a><li>Home</li></a>
-          <a><li></li></a>
           <a><li>Engagement</li></a>
         </ul>
       </div>
 
-      <div class="header__nav__boutton">
+      <div class="head__nav__right">
 
-      <button class="signup__input" type="submit">
+      <button class="head__nav__right__up" type="submit">
             <a href="pages/create-account.php"> Sign up </a>
-      </button>
-      </div>
-      <div class="header__nav__boutton">
-      <button class="signin__input" type="submit">
+      </button> 
+      <button class="head__nav_right__in" type="submit">
             <a href="pages/login.php"> Sign in </a>
       </button>
       </div>
