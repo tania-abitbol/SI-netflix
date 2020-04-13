7fd028657c376c5468b46d034699f76bc38a39bd
Guilhembozzi <guilhem.bozzi@hetic.net>
2020-04-13 14:44:34 +0200

    start landingpage

---
+++ src/pages/landingpage.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>GOFILMAX</title>
+  <link rel="stylesheet" href="../styles/css/style.css" />
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="js/index.js" defer></script>
+</head>
+
+  <body>
+<header class="header">
+    <nav class="header__nav">
+      <div class="header__nav__left">
+        <img src="../assets/images/logo.png" alt="" />
+        <ul class="header__nav__left__links">
+          <a><li>Home</li></a>
+          <a><li></li></a>
+          <a><li>engagement</li></a>
+        </ul>
+      </div>
+
+      <div class="header__nav__right">
+      </div>
+    </nav>
+  </header>
+
+
+
+
+
+  <footer class="footer">
+    <div class="footer__social-network">
+      <a href="https://twitter.com"><img class="footer__social-network__twitter"
+          src="../assets/images/social-network/twitter.svg" /></a>
+
+      <a href="https://www.facebook.com/"><img class="footer__social-network__facebook"
+          src="../assets/images/social-network/facebook.svg" /></a>
+
+      <a href="https://www.instagram.com"><img class="footer__social-network__instagram"
+          src="../assets/images/social-network/instagram.svg" /></a>
+
+      <a href="https://youtube.com"><img class="footer__social-network__youtube"
+          src="../assets/images/social-network/youtube.svg" /></a>
+    </div>
+
+    <div class="footer__rows">
+      <ul class="footer__rows__categories">
+        <li class="footer__rows__categories__item">
+          <a href="#">Audio and Subtitles</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Privacy</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+      <img
+          src="../assets/images/linefooter.svg"
+          class="footer__rows__categories--space"
+          alt="line"
+        />
+      <ul class="footer__rows__categories">
+        <li class="footer__rows__categories__item">
+          <a href="#">Audiodescription</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Investor Relations</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Legal Notices</a>
+        </li>
+      </ul>
+      <img
+          src="../assets/images/linefooter.svg"
+          class="footer__rows__categories--space"
+          alt="line"
+        />
+      <ul class="footer__rows__categories">
+        <li class="footer__rows__categories__item">
+          <a href="#">Help Center</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Jobs</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Cookie Preferences</a>
+        </li>
+      </ul>
+      <img
+          src="../assets/images/linefooter.svg"
+          class="footer__rows__categories--space"
+          alt="line"
+        />
+      <ul class="footer__rows__categories">
+        <li class="footer__rows__categories__item">
+          <a href="#">Legal Mentions</a>
+        </li>
+        <li class="footer__rows__categories__item">
+          <a href="#">Terms of Use</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+  <script src="./SI-netflix/src/js/api-imbd.js"></script>
+  <script src="./SI-netflix/src/js/slider.js"></script>
+  <script src="./SI-netflix/src/js/player.js"></script>
+</body>
+</html>
