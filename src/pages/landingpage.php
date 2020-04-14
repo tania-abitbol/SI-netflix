e54cba5af20dff6ae012bb17f4ac28134d7ea8e9
annie.trn <annietth@users.noreply.github.com>
2020-04-14 03:54:46 +0200

    Stylized header pageOne, process, and our plateform

--- src/pages/landingpage.php
+++ src/pages/landingpage.php
@@ -10,71 +10,83 @@
 
   <body>
 <header class="head">
-    <nav class="head__nav">
-      <div class="head__nav__left">
-        <img src="../assets/images/logo.png" alt="" />
-        <ul class="head__nav__left__links">
-          <a><li>Home</li></a>
-          <a><li>Engagement</li></a>
-        </ul>
-      </div>
-
-      <div class="head__nav__right">
+  <nav class="head__nav">
+    <div class="head__nav__left">
+      <img src="../assets/images/logo.png" alt="" />
+      <ul class="head__nav__left__links">
+        <a><li>Home</li></a>
+        <a><li>Engagement</li></a>
+      </ul>
+    </div>
+    <div class="head__nav__right">
 
       <button class="head__nav__right__up" type="submit">
-            <a href="pages/create-account.php"> Sign up </a>
+        <a href="../pages/create-account.php"> Sign up </a>
       </button> 
       <button class="head__nav_right__in" type="submit">
-            <a href="pages/login.php"> Sign in </a>
+        <a href="../pages/login.php"> Sign in </a>
       </button>
-      </div>
-    </nav>
-  </header>
+    </div>
+  </nav>
+</header>
+<div class="pageOne">
+  <h1 class="pageOne__title">
+    Your ideal video platform to have at home during the confinment.
+  </h1>
+  <h3>Gfx the new platform available soon !</h3>
+  <button class="pageOne__btn"type="submit">
+    <a href="#"> Press </a>
+  </button>
+</div>  
 
-  <div class="main">
-        <div class="main__box1">          
-            <h3 class="main__box1__title">GFX progress</h3>
-            <p>
-              The progress of out platform start around a single one idea, how GFX can be better than the other platform?
-            </p>
-            <p>
-              The answer turn around four fonctionalities which are important for us, for a perfect watching sessions & a magic moment
-            </p>
-            <div class="main__box1__body">
+<div class="main">
+  <div class="main__box1">          
+    <h3 class="main__box1__title">GFX progress</h3>
+    <p>
+     The progress of out platform start around a single one idea, how GFX can be better than the other platform?
+    </p>
+    <p>
+      The answer turn around four fonctionalities which are important for us, for a perfect watching sessions & a magic moment
+    </p>
+    <div class="main__box1__body">
            
-                <div class="main__box1__body__logo">
-                    <div class="main__box1__body__logo__com">
-                        <img src="../assets/images/engagement/like.svg">
-                        <p>Community</p>
-                    </div>
-                    <div class="main__box1__body__logo__pro">
-                        <img src="../assets/images/engagement/progress.svg">
-                        <p>100% connected</p>
-                    </div>
-                    <div class="main__box1__body__logo__start">
-                        <img src="../assets/images/engagement/replay_start.svg">
-                        <p>Replay Infini</p>
-                    </div>
-                    <div class="main__box1__body__logo__tchat">
-                        <img src="../assets/images/engagement/chat.svg">
-                        <p>Replay Infini</p>
-                    </div>
-                </div>
-            </div>       
+      <div class="main__box1__body__logo">
+        <div class="main__box1__body__logo__com">
+          <img src="../assets/images/engagement/like.svg">
+          <p>Community</p>
         </div>
+        <div class="main__box1__body__logo__pro">
+          <img src="../assets/images/engagement/progress.svg">
+          <p>100% connected</p>
+        </div>
+        <div class="main__box1__body__logo__start">
+          <img src="../assets/images/engagement/replay_start.svg">
+          <p>Replay Infini</p>
+        </div>
+        <div class="main__box1__body__logo__tchat">
+          <img src="../assets/images/engagement/chat.svg">
+          <p>Exchange</p>
+        </div>
+        
 
-        <div class="main__box2">
-            <h3 class="main__box2__title"> Our plateform</h3>
-            <div class="main__box2__text">
-                <p class="main__box2__text__little">We update movies that come from all over the world. For more passion  and more fun. 
-                    We know what you need and what makes you happy !
-                    Don't hesitate, it's time to take the plunge.
-                </p>
-                <img src="../assets/images/mockuup/ipad.svg">
-            </div>
-           
-            
-        <div>
+      </div>
+    </div>       
+  </div>
+
+  <div class="main__box2">
+    <h3 class="main__box2__title"> Our plateform</h3>
+    <div class="main__box2__text">
+      <div class="main__box2__text__description">
+        <p>We update movies that come from all over the world.<p>
+        <p>For more passion  and more fun. </p>
+        <p>We know what you need and what makes you happy ! <p> 
+        <p> <strong>Don't hesitate,</strong>
+          it's time to take the plunge.
+        </p>
+    </div>
+      <img src="../assets/images/mockuup/ipad.svg">
+    </div> 
+  <div>
         
         <div class="main__box3">
             <h3 class="main__box3__title">Choose your plan</h3>
